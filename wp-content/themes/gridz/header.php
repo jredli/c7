279366d941207d7b21c5ebf429d8b3f56a05209c
Redli <jovan@concept7.io>
2018-06-22 13:41:33 +0200

    Initial commit

---
+++ wp-content/themes/gridz/header.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package gridz
+ */
+?>
+<!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <title>
+        <?php
+            global $page, $paged, $gridz_options;
+            wp_title();
+        ?>
+    </title>
+    <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"/>
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<header id="header">
+    <div class="wrapper">
+        <?php gridz_site_logo(); ?>
+    </div>
+</header>
+<div id="content"<?php gridz_content_class(); ?>>
+    <div class="wrapper">
